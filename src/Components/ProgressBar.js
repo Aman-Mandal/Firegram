8d44f26529c7b0ddc0d9b4c487eaf5d6744e64d3
Aman-Mandal <002amankumar@gmail.com>
2022-07-12 17:21:02 +0530

    Progress Bar added

--- src/Components/ProgressBar.js
+++ src/Components/ProgressBar.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import useStorage from '../Hooks/useStorage'
 import styles from './ProgressBar.module.css'
 
-const ProgressBar = props => {
-  const { progress, url } = useStorage(props.file)
-  console.log(progress, url)
+const ProgressBar = ({ file, setFile }) => {
+  const { progress, url } = useStorage(file)
 
-  return <div>Progress...</div>
+  useEffect(() => {
+    if (url) {
+      setFile(null)
+    }
+  }, [url, setFile])
+
+  return (
+    <div
+      className={styles['progress-bar']}
+      style={{ width: progress + '%' }}
+    ></div>
+  )
 }
 
 export default ProgressBar
